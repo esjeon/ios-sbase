e2a1f6ba00e61e474d75214ac08ce9535fb241bb
Eon S. Jeon <esjeon@hyunmu.am>
2014-12-08 00:53:40 +0900

    use iOS SDK

--- config.mk
+++ config.mk
@@ -2,14 +2,20 @@
 VERSION = 0.0
 
 # paths
-PREFIX = /usr/local
+PREFIX = /tmp/sbase
 MANPREFIX = $(PREFIX)/share/man
 
-CC = cc
-LD = $(CC)
-AR = ar
-RANLIB = ranlib
+SDK_PATH = ../sdk
+
+CC = clang -target armv7-apple-darwin14.0.0
+LD = $(CC) -mlinker-version=1 -B$(SDK_PATH)/usr/bin
+AR = $(SDK_PATH)/usr/bin/ar
+RANLIB = $(SDK_PATH)/usr/bin/ranlib
 
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700
 CFLAGS   = -std=c99 -Wall -pedantic
-LDFLAGS  = -s
+CFLAGS  += -I$(SDK_PATH)/usr/include
+
+LDFLAGS  = 
+LDFLAGS  += -L$(SDK_PATH)/usr/lib
+LDFLAGS  += -L$(SDK_PATH)/usr/lib/system
